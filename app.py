1d4cd9c62a061efa637276e8e624a21b23894e2b
Siddharth Jain <siddharth.y.jain@oracle.com>
2018-12-24 10:19:24 +0530

    Renamed app to utils, making a flask app.py

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
-import json
-import random
-
-options_list = []
-with open('country-by-capital-city.json') as f:
-	data = json.load(f)
-print(len(data))
-answer_pos = random.randint(0, len(data))
-country = data[answer_pos]['country']
-answer = data[answer_pos]['city']
-
-print(country)
-print(answer)
-
-options_list.append(answer)
-
-def find_capital(countryname):
-	return [d for d in data if d['country']==countryname][0]['city']
-
-def mcq_options():
-	for i in range(3):
-		pos = random.randint(0, len(data))
-		options_list.append(data[pos]['city'])
-
-mcq_options()
+from flask import render_template, request, redirect, url_for
+from flask import jsonify
 
 
-random.shuffle(options_list)
-print(options_list)
-
+@app.route('/')
+def index(error=None):
+    return render_template('index.html', error=error)
