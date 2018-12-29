c439efc61c73653053dcdc5ca33ce667561d20ac
Siddharth <sidjain1412@gmail.com>
2018-12-29 11:25:42 +0530

    Not sending data to index, instead new path to send data to ajax

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from utils import option_list
 
 
@@ -7,9 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	data = option_list()
-	return render_template('index.html', data=data)
+    return render_template('index.html')
 
 
-if __name__=="__main__":
-	app.run(debug=True)
+@app.route('/getData')
+def getData():
+    data = option_list()
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
