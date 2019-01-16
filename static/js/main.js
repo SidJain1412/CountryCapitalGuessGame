a9922baa1361762f2800c5a29c6488024b9456bf
Siddharth <sidjain1412@gmail.com>
2019-01-16 14:50:15 +0530

    Using jquery to handle button clicks, code incomplete and wrong, will fix later

--- static/js/main.js
+++ static/js/main.js
@@ -6,6 +6,13 @@ var optionButtons = document.querySelectorAll("#option");
 
 getdata();
 
+
+// function buttonClick(){
+//   $("button").click(function(){
+//     console.log(but)
+//   });
+// }
+
 function getdata()
 {
 	console.log("ready");
@@ -24,21 +31,39 @@ function getdata()
 			answer = result.answer;
 			country = result.country;
 			$('#country-name').html(country);
-			for(var j = 0; j<numOptions; j++)
-			{
-				optionButtons[j].addEventListener("click", function(){
-					console.log(j)
-					console.log(optionButtons[j].innerHTML);
-					if(optionButtons[j].innerHTML == result.answer)
+			// buttonClick();
+			$(".button1").click(function(){
+					console.log("click1");
+					button = document.querySelector(".button1");
+					val = button.innerHTML;
+					console.log(val);
+					if(val == result.answer)
 					{
-						optionButtons[j].style.background == "green";
+						console.log("correct");
+						button.style.background == "green";
 					}
 					else
 					{
-						optionButtons[j].style.background == "red";
-					}
+						console.log("wrong");
+						button.style.background == "red";
+					}					
 				});
-			}
+			// for(var j = 0; j<numOptions; j++)
+			// {
+
+				// optionButtons[j].addEventListener("click", function(){
+				// 	console.log(j)
+				// 	console.log(optionButtons[j].innerHTML);
+				// 	if(optionButtons[j].innerHTML == result.answer)
+				// 	{
+				// 		optionButtons[j].style.background == "green";
+				// 	}
+				// 	else
+				// 	{
+				// 		optionButtons[j].style.background == "red";
+				// 	}
+				// });
+			// }
 		}
 	});	
 }
